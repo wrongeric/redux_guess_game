9885f48ed08d9fbeddb47733b0877d98affa7e0b
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-08 17:55:14 -0800

    finished basic game functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers';
 
 import App from './components/app';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(rootReducer)}>
+        <App />
+    </Provider>    ,
     document.getElementById('root')
 );
